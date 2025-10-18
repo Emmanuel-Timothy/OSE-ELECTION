efc028b5f7fc7d52c32884e2a34ac9ec4a2a1979
afuk08 <timothyemmanuel08@gmail.com>
2025-10-19 00:10:27 +0700

    Initial commit

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_7JLwmyBbNCx3@ep-red-wildflower-a10sulz5-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
